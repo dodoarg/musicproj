66f07d66ae4ac459cfd1d387cb5a3d3da5f6cb14
dodoarg <eargiolas96@gmail.com>
2021-12-14 10:51:23 +0100

    writes new data on existing file without overwriting it

--- song_scraper/scrape.py
+++ song_scraper/scrape.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from pathlib import Path
 
 from utils import (
     create_client,
@@ -9,12 +10,17 @@ from utils import (
     get_audio_features
 )
 
+DATA_PATH = Path('..\data')
 
 def main():
     spotify = create_client()
     n_songs = int(sys.argv[1])
-    path_dest = sys.argv[2]
-    songs = []
+    file_name = sys.argv[2]
+    path_to_file = DATA_PATH / file_name
+    if path_to_file.exists():
+        songs = json.load(open(path_to_file))
+    else:
+        songs = []
     for n in range(n_songs):
         if (n+1) % 50 == 0:
             print(f'scraping {n+1}th song...')
@@ -25,7 +31,7 @@ def main():
         musicality_features = get_musicality_features(spotify, random_song["uri"])[0]
         audio_features = get_audio_features(random_song["preview_url"])
         songs.append({**song_attributes, **musicality_features, **audio_features})
-    with open(path_dest, "w") as file:
+    with open(path_to_file, "w") as file:
         json.dump(songs, file)
 
 
