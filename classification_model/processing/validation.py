272ebfa965d45de26723d22b3c284a7e7e718f59
dodoarg <eargiolas96@gmail.com>
2021-12-20 13:38:31 +0100

    adds input data schema validation with strict types

--- classification_model/processing/validation.py
+++ classification_model/processing/validation.py
@@ -1,6 +1,7 @@
-from typing import Optional, Sequence, Tuple
+from typing import List, Optional, Sequence, Tuple
 
 import pandas as pd
+from pydantic import BaseModel, StrictFloat, StrictStr, ValidationError
 
 from classification_model.config.core import config
 
@@ -32,7 +33,58 @@ def validate_inputs(
 
     relevant_data = input_data[_relevant_features].copy()
     validated_data = drop_na_inputs(input_data=relevant_data)
-
     errors = None
 
+    try:
+        MultipleSongsDataInputs(inputs=validated_data.to_dict(orient="records"))
+
+    except ValidationError as error:
+        errors = error.errors()
+
     return validated_data, errors
+
+
+class SongsDataInputSchema(BaseModel):
+    acousticness: Optional[StrictFloat]
+    danceability: Optional[StrictFloat]
+    energy: Optional[StrictFloat]
+    instrumentalness: Optional[StrictFloat]
+    key: Optional[StrictStr]
+    liveness: Optional[StrictFloat]
+    loudness: Optional[StrictFloat]
+    mode: Optional[StrictStr]
+    speechineess: Optional[StrictFloat]
+    tempo: Optional[StrictFloat]
+    time_signature: Optional[StrictStr]
+    valence: Optional[StrictFloat]
+    beats_count: Optional[StrictFloat]
+    chroma_stft_mean: Optional[StrictFloat]
+    root_mean_square: Optional[StrictFloat]
+    spectral_centroid_mean: Optional[StrictFloat]
+    spectral_bandwidth_mean: Optional[StrictFloat]
+    rolloff_mean: Optional[StrictFloat]
+    zero_crossing_rate_mean: Optional[StrictFloat]
+    mfcc_1_mean: Optional[StrictFloat]
+    mfcc_2_mean: Optional[StrictFloat]
+    mfcc_3: Optional[StrictFloat]
+    mfcc_4_mean: Optional[StrictFloat]
+    mfcc_5_mean: Optional[StrictFloat]
+    mfcc_6_mean: Optional[StrictFloat]
+    mfcc_7_mean: Optional[StrictFloat]
+    mfcc_8_mean: Optional[StrictFloat]
+    mfcc_9_mean: Optional[StrictFloat]
+    mfcc_10_mean: Optional[StrictFloat]
+    mfcc_11_mean: Optional[StrictFloat]
+    mfcc_12_mean: Optional[StrictFloat]
+    mfcc_13_mean: Optional[StrictFloat]
+    mfcc_14_mean: Optional[StrictFloat]
+    mfcc_15_mean: Optional[StrictFloat]
+    mfcc_16_mean: Optional[StrictFloat]
+    mfcc_17_mean: Optional[StrictFloat]
+    mfcc_18_mean: Optional[StrictFloat]
+    mfcc_19_mean: Optional[StrictFloat]
+    mfcc_20_mean: Optional[StrictFloat]
+
+
+class MultipleSongsDataInputs(BaseModel):
+    inputs: List[SongsDataInputSchema]
--- tests/test_processing/test_validation.py
+++ tests/test_processing/test_validation.py
@@ -25,3 +25,30 @@ def test_validate_inputs():
     )
     pd.testing.assert_frame_equal(validated_df, exp_validated_df)
     assert errors is None
+
+    # catch pydantic validation errors
+    df_not_valid = pd.DataFrame(
+        {
+            "instrumentalness": [0.24, 0.11, -2.34],
+            "energy": ["not_a_float", 1.24, 93.23],
+            "key": ["key_0", 1, "key_2"],
+        }
+    )
+    exp_error_energy = {
+        "loc": ("inputs", 0, "energy"),
+        "msg": "value is not a valid float",
+        "type": "type_error.float",
+    }
+    exp_error_key = {
+        "loc": ("inputs", 1, "key"),
+        "msg": "str type expected",
+        "type": "type_error.str",
+    }
+    exp_error = [exp_error_energy, exp_error_key]
+    validated_df, errors = validate_inputs(
+        input_data=df_not_valid,
+        _vars_to_cast_as_obj=[],
+        _relevant_features=["instrumentalness", "energy", "key"],
+    )
+    pd.testing.assert_frame_equal(validated_df, df_not_valid)
+    assert errors == exp_error
