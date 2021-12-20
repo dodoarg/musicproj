5f3637e26c6675fd42733d540df7ed51f2ebe21a
dodoarg <eargiolas96@gmail.com>
2021-12-20 17:50:33 +0100

    fixes persisted pipeline filename; lints data_manager

--- classification_model/processing/data_manager.py
+++ classification_model/processing/data_manager.py
@@ -6,7 +6,8 @@ import pandas as pd
 from sklearn.pipeline import Pipeline
 
 from classification_model import __version__ as _version
-from classification_model.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config
+from classification_model.config.core import (DATASET_DIR, TRAINED_MODEL_DIR,
+                                              config)
 
 
 def load_dataset(*, file_name: str, _dataset_dir: Path = DATASET_DIR) -> pd.DataFrame:
@@ -18,8 +19,7 @@ def save_pipeline(
     *,
     pipeline_to_persist: Pipeline,
     _trained_model_dir: Path = TRAINED_MODEL_DIR,
-    _save_file_name: str = f"{config.app_config.pipeline_save_file} \
-        {_version}.pkl",
+    _save_file_name: str = f"{config.app_config.pipeline_save_file}{_version}.pkl",
 ) -> None:
     """Persist the pipeline.
     Saves the versioned model, and overwrites any previously
@@ -38,15 +38,14 @@ def save_pipeline(
 
 
 def load_pipeline(
-    *,
-    file_name: str,
-    _trained_model_dir: Path = TRAINED_MODEL_DIR
+    *, file_name: str, _trained_model_dir: Path = TRAINED_MODEL_DIR
 ) -> Pipeline:
     """Load a persisted pipeline"""
-    
+
     file_path = _trained_model_dir / file_name
     trained_model = joblib.load(filename=file_path)
     return trained_model
+
 
 def remove_old_pipelines(
     *, files_to_keep: List[str], _trained_model_dir: Path = TRAINED_MODEL_DIR
