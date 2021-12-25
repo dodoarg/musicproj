8338cfbcb430efe66192f116889a5081f43c5e63
dodoarg <eargiolas96@gmail.com>
2021-12-25 11:45:17 +0100

    adds health tests

---
+++ song-popularity-api/app/tests/test_api.py
@@ -0,0 +1,13 @@
+from classification_model import __version__ as model_version
+
+from app import __version__ as _api_version
+from app.config import settings
+
+def test_health(client):
+    response = client.get("http://localhost:8001/api/v1/health")
+    assert response.status_code == 200
+    health = response.json()
+    assert isinstance(health, dict)
+    assert health["name"] == settings.PROJECT_NAME
+    assert health["api_version"] == _api_version
+    assert health["model_version"] == model_version
