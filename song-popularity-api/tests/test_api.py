7a0f7545f64cc80b2ec9d8ca80deb932bae2ba7c
dodoarg <eargiolas96@gmail.com>
2021-12-26 10:45:47 +0100

    adds api error 404 tests

--- song-popularity-api/tests/test_api.py
+++ song-popularity-api/tests/test_api.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import pytest
 
 from classification_model import __version__ as model_version
 
@@ -14,6 +15,15 @@ def test_health(client):
     assert health["name"] == settings.PROJECT_NAME
     assert health["api_version"] == _api_version
     assert health["model_version"] == model_version
+
+@pytest.mark.parametrize("invalid_urls", [
+    "http//localhost:9284/api/v1/health",
+    "http//localhost:8001/api/v10/health",
+    "http//localhost:8001/api/v1/not_a_valid_endpoint",
+])
+def test_predict_error_404(client, invalid_urls):
+    response = client.post(invalid_urls)
+    assert response.status_code == 404
 
 
 def test_predict_raises_error_422(client, input_sample):
