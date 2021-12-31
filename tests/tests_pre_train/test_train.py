330a625e7102a6609f3f4bba6e0a7481c6536981
dodoarg <eargiolas96@gmail.com>
2021-12-31 10:27:50 +0100

    adds simple benchmark test

--- tests/tests_pre_train/test_train.py
+++ tests/tests_pre_train/test_train.py
@@ -1,4 +1,6 @@
+import numpy as np
 from sklearn.exceptions import NotFittedError
+from sklearn.metrics import accuracy_score
 from sklearn.pipeline import Pipeline
 
 from classification_model.training import train_pipeline_on_training_data
@@ -17,3 +19,12 @@ def test_training():
     )
     assert isinstance(fitted_pipeline, Pipeline)
     assert not isinstance(try_predict(fitted_pipeline, X_test), NotFittedError)
+
+    # benchmarking against random baseline
+    y_predicted = fitted_pipeline.predict(X_test)
+    baseline = np.random.randint(0, 2, len(y_test))
+
+    baseline_acc = accuracy_score(y_test, baseline)
+    model_acc = accuracy_score(y_test, y_predicted)
+
+    assert model_acc >= baseline_acc
