8cf620de26b765e485fb4c679c5d3d8c4a8a98c2
dodoarg <eargiolas96@gmail.com>
2022-01-02 10:47:39 +0100

    adds raise_validation_error to create_and_validate_config tests

--- tests/tests_pre_train/test_config.py
+++ tests/tests_pre_train/test_config.py
@@ -1,12 +1,34 @@
 from pathlib import Path
 
 import pytest
-from classification_model.config.core import (CONFIG_FILE_PATH, PACKAGE_ROOT,
-                                              Config,
+from classification_model.config.core import (Config,
                                               create_and_validate_config,
                                               fetch_config_from_yaml,
                                               find_config_file)
+from pydantic import ValidationError
 from strictyaml import YAML
+
+MISSING_ENTRY_TEST_CONFIG_TEXT = """
+package_name: classification_model
+
+training_data_file: train.json
+test_data_file: test.json
+test_data_predictions: test_data_predictions.csv
+
+target_int: popularity
+target_bin: is_popular
+
+pipeline_name: classification_model
+pipeline_save_file: classification_model_output_v
+
+features:
+  - feature1
+
+test_size: 0.1
+
+categorical_features:
+  - feature1
+"""
 
 
 def test_find_config_file(tmpdir):
@@ -25,11 +47,21 @@ def test_fetch_config_from_yaml(tmpdir):
     config_path = config_dir / "sample_config.txt"
     with pytest.raises(OSError):
         fetch_config_from_yaml(cfg_path=config_path)
-    
+
     config_path.write_text("this is a test")
     parsed_config = fetch_config_from_yaml(cfg_path=config_path)
     assert isinstance(parsed_config, YAML)
 
 
-def test_create_and_validate_config():
+def test_create_and_validate_config(tmpdir):
+    config_dir = Path(tmpdir)
+    config_path = config_dir / "sample_config.yml"
+    config_path.write_text(MISSING_ENTRY_TEST_CONFIG_TEXT)
+    parsed_config = fetch_config_from_yaml(config_path)
+    with pytest.raises(ValidationError) as excinfo:
+        create_and_validate_config(parsed_config=parsed_config)
+    error_msg = str(excinfo.value)
+    assert "field required" in error_msg
+    assert "random_state" in error_msg
+
     assert isinstance(create_and_validate_config(), Config)
