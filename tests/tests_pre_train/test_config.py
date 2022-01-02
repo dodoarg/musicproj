a275578955dabe70ece76395f2821b528c901b4c
dodoarg <eargiolas96@gmail.com>
2022-01-02 10:57:30 +0100

    adds test_size validator to config class, tests it

--- tests/tests_pre_train/test_config.py
+++ tests/tests_pre_train/test_config.py
@@ -30,6 +30,30 @@ categorical_features:
   - feature1
 """
 
+INVALID_ENTRY_TEST_CONFIG_TEXT = """
+package_name: classification_model
+
+training_data_file: train.json
+test_data_file: test.json
+test_data_predictions: test_data_predictions.csv
+
+target_int: popularity
+target_bin: is_popular
+
+pipeline_name: classification_model
+pipeline_save_file: classification_model_output_v
+
+features:
+  - feature1
+
+test_size: 0.05
+
+random_state: 0
+
+categorical_features:
+  - feature1
+"""
+
 
 def test_find_config_file(tmpdir):
     config_dir = Path(tmpdir)
@@ -63,5 +87,12 @@ def test_create_and_validate_config(tmpdir):
     error_msg = str(excinfo.value)
     assert "field required" in error_msg
     assert "random_state" in error_msg
+
+    config_path.write_text(INVALID_ENTRY_TEST_CONFIG_TEXT)
+    parsed_config = fetch_config_from_yaml(config_path)
+    with pytest.raises(ValidationError) as excinfo:
+        create_and_validate_config(parsed_config=parsed_config)
+    error_msg = str(excinfo.value)
+    assert "test_size must be at least 0.1" in error_msg
 
     assert isinstance(create_and_validate_config(), Config)
