1e1bc0968728fb112e98632b562a351a17cb525d
dodoarg <eargiolas96@gmail.com>
2022-01-03 12:41:29 +0100

    adds param'd tests for api error 400

--- song-popularity-api/tests/test_api.py
+++ song-popularity-api/tests/test_api.py
@@ -31,13 +31,24 @@ def test_predict_raises_error_404(client, invalid_urls):
     assert response.status_code == 404
 
 
-def test_predict_raises_error_400(client, base_url, input_sample):
+@pytest.mark.parametrize(
+    "field, value, index, error",
+    [
+        ("danceability", "not_a_number", 1, "not a valid float"),
+        ("root_mean_square_mean", "invalid", 77, "not a valid float"),
+    ],
+    ids=["feature 1", "feature 2"],
+)
+def test_predict_raises_error_400(
+    client, base_url, input_sample, caplog, field, value, index, error
+):
     payload = deepcopy(input_sample)
-    payload["inputs"][1]["danceability"] = "not_a_number"
+    payload["inputs"][index][field] = value
 
     url = f"{base_url}/predict"
     response = client.post(url, json=payload)
     assert response.status_code == 400
+    assert error in caplog.records[1].message
 
 
 def test_predict_raises_error_422(client, base_url, input_sample):
@@ -57,7 +68,7 @@ def test_predict_response_200(client, base_url, input_sample, caplog):
     payload = deepcopy(input_sample)
     url = f"{base_url}/predict"
     response = client.post(url, json=payload)
-    assert "Making predictions on inputs: " in caplog.records[0].message
+    assert "Making predictions on inputs" in caplog.records[0].message
     assert "Prediction results" in caplog.records[1].message
     assert response.status_code == 200
     prediction_data = response.json()
